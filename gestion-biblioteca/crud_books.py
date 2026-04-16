47ee9810853cb6d6f7cba96907774c8c015be147
Awpii <awpii@vivaldi.net>
2026-04-16 18:55:45 -0300

    Subo codigo viejo

---
+++ gestion-biblioteca/crud_books.py
@@ -0,0 +1,70 @@
+import gestion_datos
+import utils as utilidades
+
+def registrar_libro():
+    #Pedir datos y crear libro nuevo en books.json
+    print("\n--- Registrar nuevo libro ---")
+    titulo = input("Título: ").strip().title()
+    autor = input("Autor: ").strip().title()
+    
+    if titulo and autor:
+        libros = gestion_datos.cargar_libros()
+        nuevo_id = utilidades.generar_nuevo_id(libros, 'L')
+        nuevo_libro = {
+            "ID_Libro": nuevo_id, "Titulo": titulo, "Autor": autor,
+            "Estado": "Disponible", "Fecha_Ingreso": utilidades.obtener_fecha_actual_str()
+        }
+        libros.append(nuevo_libro)
+        if not gestion_datos.guardar_libros(libros):
+            utilidades.imprimir_error("No se pudo guardar el nuevo libro en el archivo de datos.")
+        else:
+            utilidades.imprimir_exito(f"Libro '{titulo}' registrado con ID: {nuevo_id}")
+    else:
+        utilidades.imprimir_error("Título y Autor son campos obligatorios.") #Lo mismo que en usuario, error si no agrega todos los campos
+        
+def eliminar_libro():
+    #Eliminar libro por ID, siempre que NO ESTÉ PRESTADO
+    print("\n--- Eliminar libro ---")
+    id_libro = input("Ingrese el ID del libro a eliminar (ej. L001): ").strip().upper()
+    
+    if utilidades.validar_id(id_libro, 'libro'):
+        prestamos = gestion_datos.cargar_prestamos()
+        prestamo_activo_encontrado = any(
+            p['Estado_Prestamo'] == 'Activo' and id_libro in p['ID_Libros'] for p in prestamos
+        )
+        if not prestamo_activo_encontrado:
+            libros = gestion_datos.cargar_libros()
+            longitud_inicial = len(libros)
+            libros_actualizados = list(filter(lambda l: l['ID_Libro'] != id_libro, libros)) #Filtra por el libro designado por ID para la eliminación
+            if len(libros_actualizados) < longitud_inicial:
+                if gestion_datos.guardar_libros(libros_actualizados):
+                    utilidades.imprimir_exito(f"Libro con ID {id_libro} eliminado correctamente.")
+            else:
+                utilidades.imprimir_error(f"No se encontró un libro con el ID {id_libro}.")
+        else:
+            utilidades.imprimir_error(f"No se puede eliminar el libro {id_libro}. El libro se encuentra prestado.")
+    else:
+        utilidades.imprimir_error("Formato de ID inválido.")
+
+def libros_recursividad(lista_de_libros):
+    
+    if not lista_de_libros:
+        return
+    else:
+        libro_actual = lista_de_libros[0] 
+        print(f"{libro_actual.get('ID_Libro', ''):<10} | {libro_actual.get('Titulo', ''):<40} | {libro_actual.get('Autor', ''):<25} | {libro_actual.get('Estado', ''):<12}") #se saca el primer libro de la lista
+        
+        libros_recursividad(lista_de_libros[1:]) #reinvoco la funcion de nuevo con el slice
+
+def ver_libros():
+    #Mostrar los libros por titulo.
+    print("\n--- Inventario de Libros ---")
+    libros = gestion_datos.cargar_libros()
+    
+    if not libros:
+        utilidades.imprimir_advertencia("No hay libros en el inventario.")
+    else:
+        libros_ordenados = sorted(libros, key = lambda libro: libro['ID_Libro']) #Ordena los libros por ID.
+        print(f"{'ID_Libro':<10} | {'Titulo':<40} | {'Autor':<25} | {'Estado':<12}")
+        print("-" * 80)
+        libros_recursividad(libros_ordenados)
---
+++ gestion-biblioteca/utils.py
@@ -0,0 +1,43 @@
+import re
+from datetime import datetime, timedelta
+from colorama import Fore, Style
+
+#re.compile pa compilar REGEX de una y no tener que repetir cosas ^: Inicio de string seguido de la letra del ID y \d{3}$: para que solo sean 3 numeros y fin del string
+REGEX_ID_LIBRO = re.compile(r'^L\d{3}$')
+REGEX_ID_USUARIO = re.compile(r'^U\d{3}$')
+REGEX_ID_PRESTAMO = re.compile(r'^P\d{3}$')
+
+def validar_id(id_str, tipo): #Validar el ID, por las dudas, seguro sirva para los casos de prueba
+    assert tipo in ('libro', 'usuario', 'prestamo'), "Tipo de ID no válido" #Assert para chequear que sea siempre True
+    
+    if tipo == 'libro': #.match para que todo cumpla con el REGEX
+        return REGEX_ID_LIBRO.match(id_str)
+    if tipo == 'usuario':
+        return REGEX_ID_USUARIO.match(id_str)
+    if tipo == 'prestamo':
+        return REGEX_ID_PRESTAMO.match(id_str)
+
+def generar_nuevo_id(lista_de_dicts, prefijo):#Genera IDs incrementales
+    if not lista_de_dicts:
+        return f"{prefijo}{'1'.zfill(3)}" #rellenar con 0s a la izq hasta que sean 3 digitos (i.e. nada de L02 o L2 -> siempre L002)
+    
+    clave_id = f'ID_{"Libro" if prefijo == "L" else "Usuario" if prefijo == "U" else "Prestamo"}' #defino la clave de diccionario para libros
+    numeros = list(map(lambda item: int(item[clave_id].lstrip(prefijo)), lista_de_dicts)) #lambda para sacar ID sin letra y pasar a INT
+    nuevo_numero = max(numeros) + 1 #Numero mas alto + 1
+    return f"{prefijo}{str(nuevo_numero).zfill(3)}"
+
+def obtener_fecha_actual_str(): #Funcioncita pa sacar la fecha actual con datetime, el profe dijo que podiamos usar esta lib
+    return datetime.now().strftime('%d/%m/%Y')
+
+def calcular_fecha_devolucion_str(dias = 15): #Calcula la fecha de devolucion del libro haciendo una suma, tengo que descubrir como setear la cantidad de dias
+    fecha_devolucion = datetime.now() + timedelta(days = dias)
+    return fecha_devolucion.strftime('%d/%m/%Y') 
+
+def imprimir_error(mensaje): #Colorama para que los errores sean siempre rojos, mensajes siempre verdes y warnings siempre amarillos
+    print(f"{Fore.RED}[ERROR] {mensaje}{Style.RESET_ALL}")
+
+def imprimir_exito(mensaje):
+    print(f"{Fore.GREEN}[EXITO] {mensaje}{Style.RESET_ALL}")
+
+def imprimir_advertencia(mensaje):
+    print(f"{Fore.YELLOW}[AVISO] {mensaje}{Style.RESET_ALL}")
